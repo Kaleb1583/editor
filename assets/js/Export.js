f4fcc67663754509da69f095484f175991222cff
Kaleb <74982167+Kaleb1583@users.noreply.github.com>
2024-12-21 04:45:17 -0600

    Updated Export.js

--- assets/js/Export.js
+++ assets/js/Export.js
@@ -1,5 +1,11 @@
 console.log("loaded export.")
 
+
+if(scriptsLoaded != null) {
+    scriptsLoaded.push("Export")
+}
+
+
 const save = (data, filename) => {
     const blob = new Blob([data], { type: "text/html" });
     const a = document.createElement("a");
@@ -9,14 +15,23 @@ const save = (data, filename) => {
 };
 
 function mkExport() {
-    showAlert("attempting to export", 1500, false)
+    showAlert("attempting to export", 1000, false)
     console.log("Export: attempting to export")
-    var exportCode = document.getElementById("monaco-editor").firstChild.firstChild.children[1].innerText;
-    if(exportCode != "" || String(exportCode) != templateCode) {
-        console.log("Export: showing save file in os file manager")
-        save(exportCode, "output.html");
-    } else {
-        console.log("Export: theres no code to export.")
-        showAlert("write some code before trying to export!", 3, false);
+    try {
+        var exportCode = document.getElementById("monaco-editor").firstChild.firstChild.children[1].innerText;
+        if(exportCode != "" || String(exportCode) != templateCode) {
+            console.log("Export: showing save file in os file manager")
+            save(exportCode, "output.html");
+        } else {
+            console.log("Export: theres no code to export.")
+            showAlert("write some code before trying to export!", 3, false);
+        }
+    } catch(err) {
+        if(document.getElementById("monaco-editor").firstChild == null) {      
+            setTimeout(function() {showAlert("no editor & no code to export.", 2000, false)}, 1100)
+        } else {     
+            setTimeout(function() {showAlert("Error: cant export code", 2000, false)}, 1100)
+        }
+        console.log("err making export", err)
     }
 }
